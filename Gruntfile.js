8ef67dbe4aab5cfa1916925557453ca6585d515b
b-ry <bryonurbanec@gmail.com>
2017-01-23 21:22:52 -0700

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,147 @@
+
+
+module.exports = function (grunt) {
+    var sassLib = ['bower_components'];
+
+    var jsFileList = [
+      'js/plugins/_*.js',
+      'js/plugins/*.js',
+      'js/_*.js',
+      'js/*.js'
+    ];
+
+    // Show elapsed time after tasks run to visualize performance
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+          options: {
+            jshintrc: '.jshintrc'
+          },
+          all: [
+            'Gruntfile.js',
+            'js/_*.js'
+          ]
+        },
+        concat: {
+          options: {
+            separator: ';',
+          },
+          dist: {
+            src: jsFileList,
+            dest: 'js/dist/scripts.js',
+          },
+        },
+
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
+
+        //copying bootstrap sass to _scss folder
+        copy: {
+            bootstrap: {
+                files: [
+                    {expand: true, cwd: 'bower_components/bootstrap/scss', src: ['**'], dest: '_sass/bootstrap'}
+                ]
+            }
+        },
+
+        // watch for files to change and run tasks when they do
+        watch: {
+            sass: {
+                files: ['_sass/**/*.{scss,sass}'],
+                tasks: ['sass','jshint','concat']
+            },
+            concat: {
+                files: [jsFileList],
+                tasks: ['jshint','concat']
+            }
+        },
+
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_sass',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_sass/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            },
+            deploy: {
+                files: [{
+                    expand: true,
+                    cwd: '_sass/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
+        },
+        postcss: {
+            options: {
+                map: {
+                    inline: false
+                },
+                processors: [
+                    require('pixrem')(), //adds fallbacks for rem
+                    require('autoprefixer-core')({browsers: ['last 2 versions', 'ie9']}) // add vendor prefixes
+                ]
+            },
+            build: {
+                processors: [
+                    require('pixrem')(),
+                    require('autoprefixer-core')({browsers: ['last 2 versions', 'ie9']}) // add vendor prefixes
+                ]
+            }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'jshint',
+                'concat',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
+    });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'concurrent:serve'
+    ]);
+
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild',
+        'sass',
+        'concat'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
+
+};
---
+++ README.md
@@ -0,0 +1,23 @@
+# Quick Start
+
+## Requirements
+In order to get the system up you'll need to have installed:
+
+* [node.js](https://nodejs.org/en/)
+* [grunt](http://gruntjs.com/getting-started)
+* [bower](https://bower.io/)
+
+## Installing 
+
+* Clone project
+* cd/[PROJECT]
+
+In your terminal run these commands:
+
+    1. $npm install  _You may need to sudo in order for this command to work_
+
+    2. $bower install
+
+    3. $grunt serve
+
+* Go to your browser. You should now see the site at *localhost:4000*
