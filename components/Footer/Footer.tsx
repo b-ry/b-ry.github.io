37fbd22aff5541a946ba9d985317b3c788482e5a
Bryon Urbanec <bryonurbanec@gmail.com>
2026-03-31 11:58:48 -0600

    Updating footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import Link from 'next/link'
-import styles from './footer.module.css'
+import styles from './footer.module.scss'
 
 export default function Footer() {
   return (
@@ -9,7 +9,7 @@ export default function Footer() {
         <div className={clsx(styles.contact)}>
           <p>Designed in browser and coded in <a href="https://code.visualstudio.com/" target="_blank" rel="noopener noreferrer">Visual Studio Code</a> by me. Built with <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a> and deployed with <Link href="/about">Github pages</Link>. Text is mostly set in <a href="https://fonts.google.com/specimen/Outfit" target="_blank" rel="noopener noreferrer">Outfit</a> by Google Fonts.</p>
 
-          <small className={styles.copyright}>Copyright &copy; {new Date().getFullYear()} Bryon Urbanec</small>
+          <p className={styles.copyright}>Copyright &copy; {new Date().getFullYear()} Bryon Urbanec</p>
         </div>
       </div>
     </footer>
