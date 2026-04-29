d1e4c78bb9385333b913d368642a77f9a17fa979
Bryon Urbanec <bryonurbanec@gmail.com>
2026-04-29 16:41:34 -0600

    Adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from 'next/font/local'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import '@/styles/globals.scss'
+import Script from 'next/script'
 
 const outfit = localFont({
   src: '../public/fonts/Outfit/Outfit-VariableFont_wght.ttf',
@@ -19,6 +20,19 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T3WBPKLGN8"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-T3WBPKLGN8');`,
+          }}
+        />
+      </head>
       <body className={outfit.variable}>
         <Link
           href="#content"
